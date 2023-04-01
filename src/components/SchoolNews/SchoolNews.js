dad212ca853fa8b48881c0ad592e19075f84542d
Kedmar20 <115697438+kedmar20@users.noreply.github.com>
2023-04-01 14:51:41 +0200

    add GraphQL and DatoCMS fetch method

--- src/components/SchoolNews/SchoolNews.js
+++ src/components/SchoolNews/SchoolNews.js
@@ -1,45 +1,64 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { Article, ArticleImage, Articles, Title, Wrapper } from 'components/SchoolNews/SchoolNews.styles';
 
+const DATO_TOKEN = '2b0a580c34ced15932a0d9ef47e000';
 
-const SchoolNews = () => (
-  <Wrapper>
-    <Title>Gazetka szkolna</Title>
-    <Articles>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-        <h3>Wymiana komputerów</h3>
-        <p>Już niebawem w sali informatycznej 118
-          pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-    </Articles>
-  </Wrapper>
-);
+const SchoolNews = () => {
+
+  useEffect(()=>{
+    fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers:{
+        'Authorization': `Bearer ${DATO_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query: '{allArticles {titel}}'
+      })
+    })
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+        .catch(err=>console.log(err));
+  })
+
+  return (
+      <Wrapper>
+        <Title>Schülerzeitung</Title>
+        <Articles>
+          <Article>
+            <ArticleImage>
+              <img src="https://picsum.photos/250/250" />
+            </ArticleImage>
+            <div>
+              <h3>Wymiana komputerów</h3>
+              <p>Już niebawem w sali informatycznej 118
+                pojawi się kilka nowych urządzeń!</p>
+            </div>
+          </Article>
+          <Article>
+            <ArticleImage>
+              <img src="https://picsum.photos/250/250" />
+            </ArticleImage>
+            <div>
+              <h3>Wymiana komputerów</h3>
+              <p>Już niebawem w sali informatycznej 118
+                pojawi się kilka nowych urządzeń!</p>
+            </div>
+          </Article>
+          <Article>
+            <ArticleImage>
+              <img src="https://picsum.photos/250/250" />
+            </ArticleImage>
+            <div>
+              <h3>Wymiana komputerów</h3>
+              <p>Już niebawem w sali informatycznej 118
+                pojawi się kilka nowych urządzeń!</p>
+            </div>
+          </Article>
+        </Articles>
+      </Wrapper>
+  );
+}
 
 SchoolNews.propTypes = {};
 
